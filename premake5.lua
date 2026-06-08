7501df2f1c07bbed0a0c262d4d2c3a9e1554159f
hdia1433 <procon.loz.botw@gmail.com>
2026-06-08 10:01:20 -0600

    Compiled the project into minesweeper.app

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,27 @@
+newoption {
+    trigger = "config",
+    value = "CONFIG",
+    description = "Build configuration",
+    allowed = {
+        { "debug",   "Debug build" },
+        { "release", "Release build" },
+    },
+    default = "debug",
+}
+
+local cfg = _OPTIONS["config"] or "debug"
+
 workspace("minesweeper")
 architecture("ARM64")
+if cfg == "release" then
+configurations({"Release",  "Debug"})
+else
 configurations({ "Debug", "Release" })
+end
 startproject("minesweeper")
 
 project("minesweeper")
-kind("ConsoleApp")
+kind("WindowedApp")
 language("C++")
 cppdialect("C++23")
 targetdir("bin/%{cfg.buildcfg}")
@@ -17,8 +34,22 @@ prebuildcommands({
 	"mkdir -p bin-int/Release && cp src/headers/pch.hpp bin-int/Release/pch.hpp",
 })
 
-postbuildcommands({ "ninja -t compdb > compile_commands.json" })
-
+if cfg == "release" then
+postbuildcommands({
+    "mkdir -p bin/%{cfg.buildcfg}/minesweeper.app/Contents/MacOS",
+    "mkdir -p bin/%{cfg.buildcfg}/minesweeper.app/Contents/Resources",
+    "cp bin/%{cfg.buildcfg}/minesweeper bin/%{cfg.buildcfg}/minesweeper.app/Contents/MacOS/minesweeper",
+    "cp Info.plist bin/%{cfg.buildcfg}/minesweeper.app/Contents/Info.plist",
+    "mkdir -p bin/%{cfg.buildcfg}/minesweeper.app/Contents/Frameworks",
+    "cp /opt/homebrew/Cellar/sfml/3.0.1/lib/libsfml-graphics.3.0.1.dylib bin/%{cfg.buildcfg}/minesweeper.app/Contents/Frameworks/",
+    "cp /opt/homebrew/Cellar/sfml/3.0.1/lib/libsfml-window.3.0.1.dylib bin/%{cfg.buildcfg}/minesweeper.app/Contents/Frameworks/",
+    "cp /opt/homebrew/Cellar/sfml/3.0.1/lib/libsfml-system.3.0.1.dylib bin/%{cfg.buildcfg}/minesweeper.app/Contents/Frameworks/",
+})
+else
+postbuildcommands({
+            "ninja -t compdb > compile_commands.json",
+    })
+end
 -- Source files
 files({
 	"src/**.cpp",
